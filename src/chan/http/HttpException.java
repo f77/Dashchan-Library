ab4f256299d746740f21b09450edd93ac602d0a1
Mishiranu <fukurou.mishiranu@gmail.com>
2016-05-27 21:35:53 +0300

    HttpException types

--- src/chan/http/HttpException.java
+++ src/chan/http/HttpException.java
@@ -21,22 +21,27 @@ public class HttpException extends Exception
 	}
 	
 	/**
-	 * <p>Constructor for a {@link HttpException} with response code, message and cause.</p>
-	 * 
-	 * @param responseCode Response code.
-	 * @param detailMessage Response message.
-	 */
-	public HttpException(int responseCode, String detailMessage, Throwable throwable)
-	{
-		throw new IllegalAccessError();
-	}
-	
-	/**
 	 * <p>Returns response code.</p>
 	 * 
 	 * @return Response code.
 	 */
 	public int getResponseCode()
+	{
+		throw new IllegalAccessError();
+	}
+	
+	/**
+	 * <p>Returns whether exception is HTTP protocol exception.</p>
+	 */
+	public boolean isHttpException()
+	{
+		throw new IllegalAccessError();
+	}
+	
+	/**
+	 * <p>Returns whether exception is socket level exception.</p>
+	 */
+	public boolean isSocketException()
 	{
 		throw new IllegalAccessError();
 	}
