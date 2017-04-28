0bac5602e897bb5996c88085f594fc59d6d57447
Mishiranu <fukurou.mishiranu@gmail.com>
2017-04-28 22:35:34 +0300

    Add support for typesafe TemplateParser

--- src/chan/text/TemplateParser.java
+++ src/chan/text/TemplateParser.java
@@ -7,142 +7,37 @@ package chan.text;
  * <p>You can define parsing rules using the following methods:</p>
  *
  * <ul>
- * <li>{@link #name(String)}</li>
- * <li>{@link #equals(String, String, String)}</li>
- * <li>{@link #starts(String, String, String)}</li>
- * <li>{@link #contains(String, String, String)}</li>
- * <li>{@link #ends(String, String, String)}</li>
+ * <li>{@link SimpleRuleBuilder#name(String)}</li>
+ * <li>{@link ComplexRuleBuilder#equals(String, String, String)}</li>
+ * <li>{@link ComplexRuleBuilder#starts(String, String, String)}</li>
+ * <li>{@link ComplexRuleBuilder#contains(String, String, String)}</li>
+ * <li>{@link ComplexRuleBuilder#ends(String, String, String)}</li>
  * </ul>
  *
  * <p>And define reaction rules:</p>
  *
  * <ul>
- * <li>{@link #open(OpenCallback)}</li>
- * <li>{@link #content(ContentCallback)}</li>
- * <li>{@link #close(CloseCallback)}</li>
- * <li>{@link #text(TextCallback)}</li>
+ * <li>{@link OpenBuilder#open(OpenCallback)}</li>
+ * <li>{@link ContentBuilder#content(ContentCallback)}</li>
+ * <li>{@link SimpleBuilder#close(CloseCallback)}</li>
+ * <li>{@link InitialBuilder#text(TextCallback)}</li>
  * </ul>
  *
- * <p>After defining parsing rules you should call {@link #prepare()} method. Then you can use your parsing
- * calling {@link #parse(String, Object)} method.</p>
+ * <p>After defining parsing rules you should call {@link InitialBuilder#prepare()} method.
+ * Then you can use your parsing calling {@link #parse(String, Object)} method.</p>
  */
 public final class TemplateParser<H> {
-	/**
-	 * <p>Indicates the parser to react on {@code tagName} tags.</p>
-	 *
-	 * @param tagName Tag name.
-	 * @return This parser.
-	 */
-	public TemplateParser<H> name(String tagName) {
+	TemplateParser() {
 		throw new IllegalAccessError();
 	}
 
 	/**
-	 * <p>Indicates the parser to react on {@code tagName} tags which has an {@code attribute}
-	 * equals {@code value}.</p>
+	 * <p>Creates a new parser builder.</p>
 	 *
-	 * @param tagName Tag name.
-	 * @param attribute Attribute name.
-	 * @param value Attribute value.
-	 * @return This parser.
+	 * @param <H> Holder object type.
+	 * @return Template parser builder.
 	 */
-	public TemplateParser<H> equals(String tagName, String attribute, String value) {
-		throw new IllegalAccessError();
-	}
-
-	/**
-	 * <p>Indicates the parser to react on {@code tagName} tags which has an {@code attribute}
-	 * starts with {@code value}.</p>
-	 *
-	 * @param tagName Tag name.
-	 * @param attribute Attribute name.
-	 * @param value Attribute value.
-	 * @return This parser.
-	 */
-	public TemplateParser<H> starts(String tagName, String attribute, String value) {
-		throw new IllegalAccessError();
-	}
-
-	/**
-	 * <p>Indicates the parser to react on {@code tagName} tags which has an {@code attribute}
-	 * contains {@code value}.</p>
-	 *
-	 * @param tagName Tag name.
-	 * @param attribute Attribute name.
-	 * @param value Attribute value.
-	 * @return This parser.
-	 */
-	public TemplateParser<H> contains(String tagName, String attribute, String value) {
-		throw new IllegalAccessError();
-	}
-
-	/**
-	 * <p>Indicates the parser to react on {@code tagName} tags which has an {@code attribute}
-	 * ends with {@code value}.</p>
-	 *
-	 * @param tagName Tag name.
-	 * @param attribute Attribute name.
-	 * @param value Attribute value.
-	 * @return This parser.
-	 */
-	public TemplateParser<H> ends(String tagName, String attribute, String value) {
-		throw new IllegalAccessError();
-	}
-
-	/**
-	 * <p>Defines a reaction callback when tag opened. This callback determines if parser should parse the full
-	 * tag content and call content callback or not depending on the return value. If you don't specify this callback,
-	 * parser will parser full content anyway.</p>
-	 *
-	 * @param openCallback Tag open callback.
-	 * @return This parser.
-	 * @see GroupParser.Callback#onStartElement(GroupParser, String, String)
-	 */
-	public TemplateParser<H> open(OpenCallback<H> openCallback) {
-		throw new IllegalAccessError();
-	}
-
-	/**
-	 * <p>Defines a reaction callback when full tag content parsed. This callback may be not called if open callback
-	 * returned a {@code false} value.</p>
-	 *
-	 * @param contentCallback Tag content callback.
-	 * @return This parser.
-	 * @see GroupParser.Callback#onGroupComplete(GroupParser, String)
-	 */
-	public TemplateParser<H> content(ContentCallback<H> contentCallback) {
-		throw new IllegalAccessError();
-	}
-
-	/**
-	 * <p>Defines a reaction callback when tag closed. Parser can react only on {@link #name(String)} rule.</p>
-	 *
-	 * @param closeCallback Tag close callback.
-	 * @return This parser.
-	 * @see GroupParser.Callback#onEndElement(GroupParser, String)
-	 */
-	public TemplateParser<H> close(CloseCallback<H> closeCallback) {
-		throw new IllegalAccessError();
-	}
-
-	/**
-	 * <p>Defines a reaction callback on text between tags. This callback doesn't depend on any rules.</p>
-	 *
-	 * @param textCallback Text between tags callback.
-	 * @return This parser.
-	 * @see GroupParser.Callback#onText(GroupParser, String, int, int)
-	 */
-	public TemplateParser<H> text(TextCallback<H> textCallback) {
-		throw new IllegalAccessError();
-	}
-
-	/**
-	 * <p>Normalizes a parser content and allows you to use your parser. After calling this methods you can't
-	 * define any new rules or callbacks.</p>
-	 *
-	 * @return This parser.
-	 */
-	public TemplateParser<H> prepare() {
+	public static <H> InitialBuilder<H> builder() {
 		throw new IllegalAccessError();
 	}
 
@@ -197,7 +92,7 @@ public final class TemplateParser<H> {
 	 */
 	public interface OpenCallback<H> {
 		/**
-		 * <p>Tag open callback method. See {@link #open(OpenCallback)}.</p>
+		 * <p>Tag open callback method. See {@link OpenBuilder#open(OpenCallback)}.</p>
 		 *
 		 * @param instance Parser instance holder.
 		 * @param holder Intermediate data holder.
@@ -214,7 +109,7 @@ public final class TemplateParser<H> {
 	 */
 	public interface ContentCallback<H> {
 		/**
-		 * <p>Tag full content method. See {@link #content(ContentCallback)}.</p>
+		 * <p>Tag full content method. See {@link ContentBuilder#content(ContentCallback)}.</p>
 		 *
 		 * @param instance Parser instance holder.
 		 * @param holder Intermediate data holder.
@@ -229,7 +124,7 @@ public final class TemplateParser<H> {
 	 */
 	public interface CloseCallback<H> {
 		/**
-		 * <p>Tag close callback method. See {@link #close(CloseCallback)}.</p>
+		 * <p>Tag close callback method. See {@link SimpleBuilder#close(CloseCallback)}.</p>
 		 *
 		 * @param instance Parser instance holder.
 		 * @param holder Intermediate data holder.
@@ -244,7 +139,7 @@ public final class TemplateParser<H> {
 	 */
 	public interface TextCallback<H> {
 		/**
-		 * <p>Text between tags callback method. See {@link #text(TextCallback)}.</p>
+		 * <p>Text between tags callback method. See {@link InitialBuilder#text(TextCallback)}.</p>
 		 *
 		 * @param instance Parser instance holder.
 		 * @param holder Intermediate data holder.
@@ -254,5 +149,161 @@ public final class TemplateParser<H> {
 		 * @throws ParseException to interrupt parsing process.
 		 */
 		public void onText(Instance instance, H holder, String source, int start, int end) throws ParseException;
+	}
+
+	/**
+	 * <p>Parser builder.</p>
+	 */
+	public interface SimpleRuleBuilder<H> {
+		/**
+		 * <p>Indicates the parser to react on {@code tagName} tags.</p>
+		 *
+		 * @param tagName Tag name.
+		 * @return Parser builder.
+		 */
+		public SimpleBuilder<H> name(String tagName);
+	}
+
+	/**
+	 * <p>Parser builder.</p>
+	 */
+	public interface ComplexSimpleRuleBuilder<H> {
+		/**
+		 * <p>Indicates the parser to react on {@code tagName} tags.</p>
+		 *
+		 * @param tagName Tag name.
+		 * @return Parser builder.
+		 */
+		public ComplexBuilder<H> name(String tagName);
+	}
+
+	/**
+	 * <p>Parser builder.</p>
+	 */
+	public interface ComplexRuleBuilder<H> {
+		/**
+		 * <p>Indicates the parser to react on {@code tagName} tags which has an {@code attribute}
+		 * equals {@code value}.</p>
+		 *
+		 * @param tagName Tag name.
+		 * @param attribute Attribute name.
+		 * @param value Attribute value.
+		 * @return Parser builder.
+		 */
+		public ComplexBuilder<H> equals(String tagName, String attribute, String value);
+
+		/**
+		 * <p>Indicates the parser to react on {@code tagName} tags which has an {@code attribute}
+		 * starts with {@code value}.</p>
+		 *
+		 * @param tagName Tag name.
+		 * @param attribute Attribute name.
+		 * @param value Attribute value.
+		 * @return Parser builder.
+		 */
+		public ComplexBuilder<H> starts(String tagName, String attribute, String value);
+
+		/**
+		 * <p>Indicates the parser to react on {@code tagName} tags which has an {@code attribute}
+		 * contains {@code value}.</p>
+		 *
+		 * @param tagName Tag name.
+		 * @param attribute Attribute name.
+		 * @param value Attribute value.
+		 * @return Parser builder.
+		 */
+		public ComplexBuilder<H> contains(String tagName, String attribute, String value);
+
+		/**
+		 * <p>Indicates the parser to react on {@code tagName} tags which has an {@code attribute}
+		 * ends with {@code value}.</p>
+		 *
+		 * @param tagName Tag name.
+		 * @param attribute Attribute name.
+		 * @param value Attribute value.
+		 * @return Parser builder.
+		 */
+		public ComplexBuilder<H> ends(String tagName, String attribute, String value);
+	}
+
+	/**
+	 * <p>Parser builder.</p>
+	 */
+	public interface OpenBuilder<H> {
+		/**
+		 * <p>Defines a reaction callback when tag opened. This callback determines whether parser should parse
+		 * the full tag content and call content callback or not depending on the return value. If you don't
+		 * specify this callback parser will parse full content anyway.</p>
+		 *
+		 * @param openCallback Tag open callback.
+		 * @return Parser builder.
+		 * @see GroupParser.Callback#onStartElement(GroupParser, String, String)
+		 */
+		public ContentBuilder<H> open(OpenCallback<H> openCallback);
+
+		/**
+		 * <p>Defines a reaction callback when full tag content parsed. This callback may be not called if
+		 * open callback returned a {@code false} value.</p>
+		 *
+		 * @param contentCallback Tag content callback.
+		 * @return Parser builder.
+		 * @see GroupParser.Callback#onGroupComplete(GroupParser, String)
+		 */
+		public InitialBuilder<H> content(ContentCallback<H> contentCallback);
+	}
+
+	/**
+	 * <p>Parser builder.</p>
+	 */
+	public interface InitialBuilder<H> extends SimpleRuleBuilder<H>, ComplexRuleBuilder<H> {
+		/**
+		 * <p>Defines a reaction callback on text between tags. This callback doesn't depend on any rules.</p>
+		 *
+		 * @param textCallback Text between tags callback.
+		 * @return Parser builder.
+		 * @see GroupParser.Callback#onText(GroupParser, String, int, int)
+		 */
+		public InitialBuilder<H> text(TextCallback<H> textCallback);
+
+		/**
+		 * <p>Creates a parser from builder.</p>
+		 *
+		 * @return Parser instance.
+		 */
+		public TemplateParser<H> prepare();
+	}
+
+	/**
+	 * <p>Parser builder.</p>
+	 */
+	public interface SimpleBuilder<H> extends SimpleRuleBuilder<H>, ComplexRuleBuilder<H>, OpenBuilder<H> {
+		/**
+		 * <p>Defines a reaction callback when tag closed. Parser can react only on {@link #name(String)} rule.</p>
+		 *
+		 * @param closeCallback Tag close callback.
+		 * @return Parser builder.
+		 * @see GroupParser.Callback#onEndElement(GroupParser, String)
+		 */
+		public InitialBuilder<H> close(CloseCallback<H> closeCallback);
+	}
+
+	/**
+	 * <p>Parser builder.</p>
+	 */
+	public interface ComplexBuilder<H> extends ComplexSimpleRuleBuilder<H>, ComplexRuleBuilder<H>, OpenBuilder<H> {}
+
+	/**
+	 * <p>Parser builder.</p>
+	 */
+	public interface ContentBuilder<H> extends InitialBuilder<H> {
+		/**
+		 * <p>Defines a reaction callback when full tag content parsed. This callback may be not called if
+		 * open callback returned a {@code false} value.</p>
+		 *
+		 * @param contentCallback Tag content callback.
+		 * @return Parser builder.
+		 * @see GroupParser.Callback#onGroupComplete(GroupParser, String)
+		 */
+		public InitialBuilder<H> content(ContentCallback<H> contentCallback);
 	}
 }
